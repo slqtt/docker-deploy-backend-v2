7e4a5c4082ef978823cee2d8df14a7f1f55896a2
Ubuntu <ubuntu@ip-172-31-21-193.ec2.internal>
2026-06-09 14:53:59 +0000

    feat: new grading criteria (90→A, stricter)

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -6,18 +6,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class GradeServiceImpl implements GradeService {
     @Override
-    public String getGrade(double score) {
-        if (score > 79.5) {
-            return "A";
-        }
-        else if (score > 74.5){
-            return "B";
-        }else if (score > 59.5){
-            return "C";
-        }else if (score > 32.5) {
-            return "D";
-        } else
-            return "F";
-
+public String getGrade(double score) {
+    if (score >= 90）{
+        return "A";
+    } else if (score >= 80) {
+        return "B";
+    } else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else {
+        return "F";
     }
 }
